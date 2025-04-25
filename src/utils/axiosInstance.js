03f32fe0cfd777234839a604c0acb2687ee07bd5
RajkumarMM <mmraj816@gmail.com>
2025-04-25 19:19:00 +0530

    backend url changed

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000/api", // Central base URL
+  baseURL: "https://hrmsbackend-ueha.onrender.com/api", // Central base URL
 });
 
 export default axiosInstance;
